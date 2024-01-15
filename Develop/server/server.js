b7e8b83eed76e20ffd4d3145a3f8610c0c74eec7
Jason Grant <jason82autotech@gmail.com>
2024-01-15 15:37:26 -0800

    installed apollo server and modified server.js

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+const typeDefs = require('./schemas');
+const resolvers = require('./resolvers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,8 +16,18 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// Create an ApolloServer instance with your type definitions and resolvers
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Apply ApolloServer to Express as middleware
+server.applyMiddleware({ app });
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`🚀 Apollo Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
